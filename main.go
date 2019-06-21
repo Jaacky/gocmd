da6dede16b4c1af6b31f672693a427f3ab3b9de6
Jacky <jaackyvan@gmail.com>
2019-06-20 22:37:31 -0700

    Adding a WIP create dir API call

--- main.go
+++ main.go
@@ -49,3 +49,36 @@ func listContents() {
 	body, _ := ioutil.ReadAll(resp.Body)
 	log.Println(string([]byte(body)))
 }
+
+func createFolder() {
+	dropboxAccessToken := os.Getenv(DropboxAccessTokenEnv)
+	url := "https://api.dropboxapi.com/2/files/create_folder_v2"
+
+	requestBody, err := json.Marshal(map[string]string{
+		"path": "/Test/Inside",
+	})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Create a Bearer string by appending string access token
+	bearer := "Bearer " + dropboxAccessToken
+
+	// Create a new request using http
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(requestBody))
+
+	// add authorization header to the req
+	req.Header.Add("Authorization", bearer)
+	req.Header.Add("Content-Type", "application/json")
+
+	// Send req using http Client
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("Error on response.\n[ERRO] -", err)
+	}
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	log.Println(string([]byte(body)))
+}
