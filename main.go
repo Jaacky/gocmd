eabe09835d41aaa4ec096c734be3325fc1ee1773
Jacky <jaackyvan@gmail.com>
2019-06-23 15:01:04 -0700

    Extracting Dropbox funcs into its own package

--- main.go
+++ main.go
@@ -1,95 +1,9 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
-)
-
-const (
-	// DropboxAccessTokenEnv is the ENV var name for the Dropbox access token
-	DropboxAccessTokenEnv = "DROPBOX_ACCESS_TOKEN"
+	"github.com/Jaacky/gocmd/dropbox"
 )
 
 func main() {
-	listContents()
-}
-
-type requestListFolder struct {
-	Path      string `json:"path"`
-	Recursive bool   `json:"recursive"`
-}
-
-func createNewRequestListFolder(path string, recursive bool) *requestListFolder {
-	return &requestListFolder{
-		Path:      path,
-		Recursive: recursive,
-	}
-}
-
-func listContents() {
-	dropboxAccessToken := os.Getenv(DropboxAccessTokenEnv)
-	url := "https://api.dropboxapi.com/2/files/list_folder"
-
-	requestBody, err := json.Marshal(createNewRequestListFolder("", true))
-
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	// Create a Bearer string by appending string access token
-	bearer := "Bearer " + dropboxAccessToken
-
-	// Create a new request using http
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(requestBody))
-
-	// add authorization header to the req
-	req.Header.Add("Authorization", bearer)
-	req.Header.Add("Content-Type", "application/json")
-
-	// Send req using http Client
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Println("Error on response.\n[ERRO] -", err)
-	}
-
-	body, _ := ioutil.ReadAll(resp.Body)
-	log.Println(string([]byte(body)))
-}
-
-func createFolder() {
-	dropboxAccessToken := os.Getenv(DropboxAccessTokenEnv)
-	url := "https://api.dropboxapi.com/2/files/create_folder_v2"
-
-	requestBody, err := json.Marshal(map[string]string{
-		"path": "/Test/Inside",
-	})
-
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	// Create a Bearer string by appending string access token
-	bearer := "Bearer " + dropboxAccessToken
-
-	// Create a new request using http
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(requestBody))
-
-	// add authorization header to the req
-	req.Header.Add("Authorization", bearer)
-	req.Header.Add("Content-Type", "application/json")
-
-	// Send req using http Client
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Println("Error on response.\n[ERRO] -", err)
-	}
-
-	body, _ := ioutil.ReadAll(resp.Body)
-	log.Println(string([]byte(body)))
+	dropbox.ListContents()
 }
