f8c48ce80e3b05ad9212b3c023df45de099b23e6
Jacky <jaackyvan@gmail.com>
2019-07-07 12:45:32 -0700

    Handling sync command to upload keyfile

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"os"
+	"strings"
+
 	"github.com/Jaacky/gocmd/dropbox"
 )
 
+const (
+	keyfileEnv = "KEYFILE"
+)
+
 func main() {
-	dropbox.ListContents()
+	args := os.Args
+
+	if len(args) >= 2 && os.Args[1] == "sync" {
+		keyfile := os.Getenv(keyfileEnv)
+
+		split := strings.Split(keyfile, "/")
+		fileName := split[len(split)-1]
+
+		dropbox.UploadFile(keyfile, "/"+fileName)
+	}
 }
